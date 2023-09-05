248850936d11d084e2543fccf9c7cbccda9bf992
winsparc <winsparc@gmail.com>
2023-09-05 12:21:59 +0530

    'wraptron-community'

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -6,6 +6,7 @@ import { Tab2Page } from './tab2.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab2PageRoutingModule } from './tab2-routing.module';
+import {MediaCapture} from '@ionic-native/media-capture/ngx'
 
 @NgModule({
   imports: [
@@ -15,6 +16,7 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
     ExploreContainerComponentModule,
     Tab2PageRoutingModule
   ],
-  declarations: [Tab2Page]
+  declarations: [Tab2Page],
+  providers:[MediaCapture]
 })
 export class Tab2PageModule {}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {MediaCapture,MediaFile,CaptureAudioOptions} from '@ionic-native/media-capture/ngx'
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -7,6 +7,31 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(
+    private mediaCapture: MediaCapture,
+  ) {}
+
+  async startRecording() {
+     
+    var options:CaptureAudioOptions={
+     limit:1
+    }
+    this.mediaCapture.captureAudio(options).then((mediaFiles: MediaFile[]) => {
+      // Success case: mediaFiles is an array of captured video files
+      if (mediaFiles.length > 0) {
+        const firstVideoFile = mediaFiles[0]; 
+        alert(firstVideoFile.fullPath)// Assuming you want to work with the first captured video
+        console.log('Captured video:', firstVideoFile.fullPath);
+        // Your further processing logic for successful capture
+      } else {
+        alert('error')
+        console.error('No video files captured.');
+        // Handle the case where no video files were captured
+      }
+    })
+  }
+
+
+
 
 }
